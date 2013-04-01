6f20cc523952c9b7e2cb6fe42db08a81a29fcae7
Laurent Constantin <github@blackcrowsteam.com>
2013-04-01 16:08:43 +0200

    logo

--- google-play/images.sh
+++ google-play/images.sh
@@ -2,18 +2,18 @@
 # Please install ImageMagick to convert images !
 # Laurent Constantin
 
-convert simpleicon512.png -resize 80x80 ../res/drawable-hdpi/simpleicon.png
-convert simpleicon512.png -resize 96x96 ../res/drawable-mdpi/simpleicon.png
-convert simpleicon512.png -resize 96x96 ../res/drawable-xhdpi/simpleicon.png
+convert SVG:musicstopsimple.svg -resize 80x80 ../res/drawable-hdpi/simpleicon.png
+convert SVG:musicstopsimple.svg -resize 96x96 ../res/drawable-mdpi/simpleicon.png
+convert SVG:musicstopsimple.svg -resize 96x96 ../res/drawable-xhdpi/simpleicon.png
 
-convert simpleicon512.png -resize 300x300 ../res/drawable-hdpi/musicstop_big.png
-convert simpleicon512.png -resize 200x200 ../res/drawable-mdpi/musicstop_big.png
-convert simpleicon512.png -resize 400x400 ../res/drawable-xhdpi/musicstop_big.png
+convert SVG:musicstopsimple.svg -size 300x300 ../res/drawable-hdpi/musicstop_big.png
+convert SVG:musicstopsimple.svg -size 200x200 ../res/drawable-mdpi/musicstop_big.png
+convert SVG:musicstopsimple.svg -size 400x400 ../res/drawable-xhdpi/musicstop_big.png
 
 
-convert icon512.png -resize 100x100 icon.png
+convert SVG:musicstopsimple.svg -resize 100x100 icon.png
 
-convert icon512.png -resize 72x72 ../res/drawable-hdpi/ic_launcher.png
-convert icon512.png -resize 48x48 ../res/drawable-mdpi/ic_launcher.png
-convert icon512.png -resize 96x96 ../res/drawable-xhdpi/ic_launcher.png
+convert SVG:musicstopsimple.svg -resize 72x72 ../res/drawable-hdpi/ic_launcher.png
+convert SVG:musicstopsimple.svg -resize 48x48 ../res/drawable-mdpi/ic_launcher.png
+convert SVG:musicstopsimple.svg -resize 96x96 ../res/drawable-xhdpi/ic_launcher.png
 
